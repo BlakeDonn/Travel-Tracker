1c3eaf9a9071079ae73b388da3dae989f0ccd6c4
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-09-19 17:48:42 -0400

    Refactor bad info test

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,17 +1,36 @@
 import chai from 'chai';
 import Traveler from '../src/classes/traveler';
 const expect = chai.expect;
-let traveler, travelerInfo;
+let traveler, travelerInfo, tripInfo;
 
 describe('See if the tests are running', function() {
-    
   beforeEach(() => {
     travelerInfo = {
       "id": 6,
       "name": "Blake D",
       "travelerType": "history buff"
     }
+    tripInfo = 
+        [{
+          "id": 1,
+          "userID": 6,
+          "destinationID": 49,
+          "date": "2019/09/16",
+        },
+        {
+          "id": 1,
+          "userID": 32,
+          "destinationID": 49,
+          "date": "2019/09/16",
+        },
+        {
+          "id": 1,
+          "userID": 45,
+          "destinationID": 49,
+          "date": "2019/09/16",
+        }]
     traveler = new Traveler(travelerInfo);
+
   });
 
   it('Should be a function', () => {
@@ -27,12 +46,9 @@ describe('See if the tests are running', function() {
   });
 
   it('Should not store bad info', () => {
-    let badInfo = {
-      "id": '6',
-      "name": 43,
-      "travelerType": 52
-    }
-    let testTraveler = new Traveler(badInfo)
+    travelerInfo.id = '6',  travelerInfo. name = 43, travelerInfo.travelerType = 52;
+    let testTraveler = new Traveler(travelerInfo)
     expect(testTraveler.userInfo).to.equal(null)
   });
+  
 });
