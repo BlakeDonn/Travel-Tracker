4301a7d3f4c2e13d011d88be13442828b02aa0b1
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-09-19 17:30:03 -0400

    Refactor userInfo to refine travelerTypes

--- src/classes/traveler.js
+++ src/classes/traveler.js
@@ -3,8 +3,8 @@ class Traveler {
   constructor(userInfo, trips) {
     this.userInfo = 
     typeof(userInfo.id) === 'number'
-      ?  typeof(userInfo.name) === 'string' 
-        ?  typeof(userInfo.travelerType) === 'string' 
+      ?  typeof(userInfo.name) === 'string' && userInfo.name.split(' ').length === 2
+        ?  typeof(userInfo.travelerType) === 'string' && 'relaxer, thrill-seeker, history buff, foodie, photographer'.includes(userInfo.travelerType)
           ? userInfo : null : null : null;
     this.trips = trips;
   }
