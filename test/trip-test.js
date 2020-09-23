7ff2979101dd0c9f61735a4b17e6a3f23d9fad3d
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-09-22 22:33:18 -0400

    Final refactor

--- test/trip-test.js
+++ test/trip-test.js
@@ -3,7 +3,7 @@ import Trip from '../src/classes/trip';
 const expect = chai.expect;
 let trip, tripInfo, dummyDesti;
 
-describe.only('See if the tests are running', function() {
+describe('See if the tests are running', function() {
   beforeEach(() => {
     tripInfo = {
       "id": 6,
