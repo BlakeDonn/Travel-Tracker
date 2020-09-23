017771df64cb647bf576b7ef119c45e9116f9f14
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-09-22 22:32:09 -0400

    Final refactor

--- test/time-test.js
+++ test/time-test.js
@@ -11,17 +11,4 @@ describe('Time', () => {
       expect(time.daysFromDate(date, -1).getTime()).to.equal(yesterday.getTime());
     });
   });
-  describe('isBetween()', () => {
-    it('should return `true` if a date falls between 2 dates (inclusive)', () => {
-      expect(time.isBetween(yesterday, date, tomorrow)).to.equal(true);
-      expect(time.isBetween(yesterday, yesterday, tomorrow)).to.equal(true);
-      expect(time.isBetween(yesterday, tomorrow, tomorrow)).to.equal(true);
-    });
-    it('should return `false` if a date falls between 2 dates (inclusive)', () => {
-      let oneMSBeforeYesterday = new Date( yesterday.getTime() - 1);
-      let oneMSAfterTomorrow = new Date ( tomorrow.getTime() + 1);
-      expect(time.isBetween(yesterday, oneMSBeforeYesterday, tomorrow)).to.equal(false);
-      expect(time.isBetween(yesterday, oneMSAfterTomorrow, tomorrow)).to.equal(false);
-    });
-  });
 });
--- test/trip-test.js
+++ test/trip-test.js
@@ -1,9 +1,9 @@
 import chai from 'chai';
 import Trip from '../src/classes/trip';
 const expect = chai.expect;
-let trip, tripInfo;
+let trip, tripInfo, dummyDesti;
 
-describe ('See if the tests are running', function() {
+describe.only('See if the tests are running', function() {
   beforeEach(() => {
     tripInfo = {
       "id": 6,
@@ -14,8 +14,14 @@ describe ('See if the tests are running', function() {
       "duration": 9,
       "status": "approved",
       "suggestedActivities": []
-    },
-    trip = new Trip(tripInfo);
+    }
+    dummyDesti = {
+        "id": 40,
+        "destination": "Blake's house",
+        "estimatedLodgingCostPerDay": 600,
+        "estimatedFlightCostPerPerson": 80
+    }
+    trip = new Trip(tripInfo, 500, dummyDesti);
   });
 
   it('Should be a function', () => {
@@ -26,8 +32,16 @@ describe ('See if the tests are running', function() {
     expect(trip).to.be.an.instanceof(Trip)
   });
 
-  it('Should set trip year on instantiation', () => {
-    expect(trip.year).to.be.equal('2020')
+  it('Should store a Date', () => {
+    expect(trip.date.toString()).to.include('2020')
+  });
+
+  it('Should store an time to evaluate with', () => {
+    expect(typeof(trip.time)).to.equal('number')
+  });
+
+  it('Should be able to evaluate trip duration', () => {
+    expect(trip.duration.toString()).to.include('2020')
   });
 
   it('Should evaluate present status correctly', () => {
@@ -36,20 +50,24 @@ describe ('See if the tests are running', function() {
 
   it('Should evaluate past status correctly', () => {
     tripInfo.date = '2020/07/20'
-    let newTrip = new Trip(tripInfo)
+    let newTrip = new Trip(tripInfo, 500, dummyDesti)
     expect(newTrip.tripStatus).to.be.equal('past')
   });
 
   it('Should evaluate present status correctly', () => {
     tripInfo.date = '2020/11/20'
-    let newTrip = new Trip(tripInfo)
+    let newTrip = new Trip(tripInfo, 500, dummyDesti)
     expect(newTrip.tripStatus).to.be.equal('upcoming')
   });
 
   it('Should evaluate pending status correctly', () => {
     tripInfo.status = 'pending'
-    let newTrip = new Trip(tripInfo)
+    let newTrip = new Trip(tripInfo, 500, dummyDesti)
     expect(newTrip.tripStatus).to.be.equal('pending')
+  });
+
+  it('Should store a trip destination', () => {
+    expect(Object.keys(trip.destination)).to.eql(['location', 'image', 'alt'])
   });
 
 });
